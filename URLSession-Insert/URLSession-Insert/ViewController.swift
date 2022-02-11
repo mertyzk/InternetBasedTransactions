3df8b1faadc7697c3c52b15d977b4d0bfc32b19e
RinKusChi <meyuzak@gmail.com>
2022-02-11 12:19:45 +0300

    API + CRUD

--- URLSession-Insert/URLSession-Insert/ViewController.swift
+++ URLSession-Insert/URLSession-Insert/ViewController.swift
@@ -11,13 +11,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        add()
+        //add()
+        //delete()
+        update()
     }
     
     func add(){
         var request = URLRequest(url: URL(string: "https://localhost:15139/api/colors/add")!) // ReCapProject API
         request.httpMethod = "POST"
-        //request.addValue("application/json", forHTTPHeaderField: "Content-type")
         let post = "colorName=SwiftTestColor"
         request.httpBody = post.data(using: .utf8)
         
@@ -36,11 +37,60 @@ class ViewController: UIViewController {
                 print(error.localizedDescription)
             }
         }.resume()
-        
-        
-        
     }
-
-
+    
+    func delete(){
+        var request = URLRequest(url: URL(string: "https://localhost:15139/api/colors/delete")!) // ReCapProject API
+        request.httpMethod = "POST"
+        let post = "kisi_id=3569"
+        request.httpBody = post.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: request) { (data,response,error) in
+            
+            if error != nil || data == nil{
+                print("Error")
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any]{
+                    print(json)
+                }
+            } catch  {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+    
+    func update(){
+        var request = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/kisiler/update_kisiler.php")!) // ReCapProject API
+        request.httpMethod = "POST"
+        let post = "kisi_id=3566&kisi_ad=NEWNAME&kisi_tel=1231231313123"
+        request.httpBody = post.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: request) { (data,response,error) in
+            
+            if error != nil || data == nil{
+                print("Error")
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any]{
+                    print(json)
+                }
+            } catch  {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
