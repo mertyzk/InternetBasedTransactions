71ffa94280a41c496c4fa602be497c510debe5c2
RinKusChi <meyuzak@gmail.com>
2022-02-20 10:18:37 +0300

    20 February 2022

--- SnapchatFirebaseClone/SnapchatFirebaseClone/ViewControllers/SnapVC.swift
+++ SnapchatFirebaseClone/SnapchatFirebaseClone/ViewControllers/SnapVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ImageSlideshow
 
 class SnapVC: UIViewController {
 
@@ -13,12 +14,35 @@ class SnapVC: UIViewController {
     
     var selectedSnap : Snap?
     var selectedTime : Int?
+    var inputArray = [KingfisherSource]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if let time = selectedTime {
             timeLeftLabel.text = "Time left: \(time)"
         }
+        
+        if let snap = selectedSnap{
+            for image in snap.imageUrlAray{
+                inputArray.append(KingfisherSource(urlString: image)!)
+            }
+            
+            let imageSlideShow = ImageSlideshow(frame: CGRect(x: 10, y: 10, width: self.view.frame.width * 0.95, height: self.view.frame.height * 0.90))
+            imageSlideShow.backgroundColor = UIColor.white
+            let pageIndicator = UIPageControl()
+            pageIndicator.currentPageIndicatorTintColor = UIColor.lightGray
+            imageSlideShow.pageIndicator = pageIndicator
+            imageSlideShow.contentScaleMode = UIViewContentMode.scaleAspectFit
+            imageSlideShow.setImageInputs(inputArray)
+            self.view.addSubview(imageSlideShow)
+            self.view.bringSubviewToFront(timeLeftLabel)
+            
+            
+            
+        }
+    
+        
+        
         
     }
     
